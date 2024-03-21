c7c09a8650de969770b0d14d0bfab5fee4a0bb45
mdfaique <mdfaique5@gmail.com>
2024-03-21 23:44:39 +0530

    Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Profile from './assets/pages/Profile'
 import SignIn from './assets/pages/SignIn'
 import SignUp from './assets/pages/SignUp'
 import About from './assets/pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
    <Route path='/' element={<Home/>}/>
    <Route path='/sign-in' element={<SignIn/>}/>
